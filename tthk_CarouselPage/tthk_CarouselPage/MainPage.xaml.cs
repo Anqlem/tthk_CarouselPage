22e3810adea6738ac6c27e8fbccd1eaa00c0ce83
Миша Шмелёв <cryptecker@hotmail.com>
2020-12-17 09:58:21 +0200

    Changes

--- tthk_CarouselPage/tthk_CarouselPage/MainPage.xaml.cs
+++ tthk_CarouselPage/tthk_CarouselPage/MainPage.xaml.cs
@@ -38,6 +38,127 @@ namespace tthk_CarouselPage
                 Text = "HDD"
             };
 
+            Label gpuL = new Label
+            {
+                Text = "Видеокарта",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Image gpuI = new Image
+            {
+                Source = "gpu.jpg",
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Label caseL = new Label
+            {
+                Text = "Корпуса",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Image caseI = new Image
+            {
+                Source = "pccase.jpg",
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Label moboL = new Label
+            {
+                Text = "Материнские Платы",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Image moboI = new Image
+            {
+                Source = "mobo.png",
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Label cpuL = new Label
+            {
+                Text = "Процессоры",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Image cpuI = new Image
+            {
+                Source = "cpu.jpg",
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Label ramL = new Label
+            {
+                Text = "ОЗУ",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Image ramI = new Image
+            {
+                Source = "ram.jpg",
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            Label hddL = new Label
+            {
+                Text = "HDD",
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Image hddI = new Image
+            {
+                Source = "hdd.jpg",
+                WidthRequest = 200,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+
+
+
+            var tapImage = new TapGestureRecognizer();
+            var tapImage1 = new TapGestureRecognizer();
+            var tapImage2 = new TapGestureRecognizer();
+            var tapImage3 = new TapGestureRecognizer();
+            var tapImage4 = new TapGestureRecognizer();
+            var tapImage5 = new TapGestureRecognizer();
+
+               
+            caseI.GestureRecognizers.Add(tapImage);
+            moboI.GestureRecognizers.Add(tapImage1);
+            gpuI.GestureRecognizers.Add(tapImage2);
+            cpuI.GestureRecognizers.Add(tapImage3);
+            ramI.GestureRecognizers.Add(tapImage4);
+            hddI.GestureRecognizers.Add(tapImage5);
+
+            tapImage.Tapped += tapImage_Tapped;
+            tapImage1.Tapped += tapImage1_Tapped;
+            tapImage2.Tapped += tapImage2_Tapped;
+            tapImage3.Tapped += tapImage3_Tapped;
+            tapImage4.Tapped += tapImage4_Tapped;
+            tapImage5.Tapped += tapImage5_Tapped;
+
             btn.Clicked += Btn_Clicked;
             btn1.Clicked += Btn1_Clicked;
             btn2.Clicked += Btn2_Clicked;
@@ -51,20 +172,10 @@ namespace tthk_CarouselPage
                 {
                     Children =
                     {
-                    new Label
-                    {
-                        Text="Корпуса",
-                        FontSize = Device.GetNamedSize(NamedSize.Medium,typeof(Label)),
-                        HorizontalOptions=LayoutOptions.Center },
-                    new Image
-                    {
-                        Source="pccase.jpg",
-                        WidthRequest=200,
-                        HeightRequest=200,
-                        HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand
-                    },
-                    btn }
+                        caseL,
+                        caseI,
+                        btn 
+                    }
 
                 }
             };
@@ -74,21 +185,10 @@ namespace tthk_CarouselPage
                 {
                     Children =
                     {
-                    new Label
-                    {
-                        Text="Материнские Платы",
-                        FontSize = Device.GetNamedSize(NamedSize.Medium,typeof(Label)),
-                        HorizontalOptions=LayoutOptions.Center },
-                    new Image
-                    {
-                        Source="mobo.png",
-                        WidthRequest=200,
-                        HeightRequest=200,
-                        HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand
-                    },
-
-                    btn1 }
+                        moboL,
+                        moboI,
+                        btn1 
+                    }
 
                 }
             };
@@ -98,19 +198,10 @@ namespace tthk_CarouselPage
                 {
                     Children =
                     {
-                    new Label
-                    {
-                        Text="Видеокарта",
-                        FontSize = Device.GetNamedSize(NamedSize.Medium,typeof(Label)),
-                        HorizontalOptions=LayoutOptions.Center },
-                    new Image
-                    {
-                        Source="gpu.jpg",
-                        WidthRequest=200,
-                        HeightRequest=200,
-                        HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand },
-                   btn2 }
+                        gpuL,
+                        gpuI,
+                        btn2 
+                    }
 
                 }
             };
@@ -120,19 +211,10 @@ namespace tthk_CarouselPage
                 {
                     Children =
                     {
-                    new Label
-                    {
-                        Text="Процессоры",
-                        FontSize = Device.GetNamedSize(NamedSize.Medium,typeof(Label)),
-                        HorizontalOptions=LayoutOptions.Center },
-                    new Image
-                    {
-                        Source="cpu.jpg",
-                        WidthRequest=200,
-                        HeightRequest=200,
-                        HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand },
-                    btn3}
+                        cpuL,
+                        cpuI,
+                        btn3
+                    }
 
                 }
             };
@@ -142,20 +224,10 @@ namespace tthk_CarouselPage
                 {
                     Children =
                     {
-                    new Label
-                    {
-                        Text="ОЗУ",
-                        FontSize = Device.GetNamedSize(NamedSize.Medium,typeof(Label)),
-                        HorizontalOptions=LayoutOptions.Center },
-                    new Image
-                    {
-                        Source="ram.jpg",
-                        WidthRequest=200,
-                        HeightRequest=200,
-                        HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand },
-                    btn4}
-
+                        ramL,
+                        ramI,
+                        btn4
+                    }
                 }
             };
             var HDD = new ContentPage
@@ -164,19 +236,10 @@ namespace tthk_CarouselPage
                 {
                     Children =
                     {
-                    new Label
-                    {
-                        Text="HDD",
-                        FontSize = Device.GetNamedSize(NamedSize.Medium,typeof(Label)),
-                        HorizontalOptions=LayoutOptions.Center },
-                    new Image
-                    {
-                        Source="hdd.jpg",
-                        WidthRequest=200,
-                        HeightRequest=200,
-                        HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand },
-                    btn5}
+                        hddL,
+                        hddI,
+                        btn5
+                    }
 
                 }
             };
@@ -222,6 +285,30 @@ namespace tthk_CarouselPage
         {
             Uri uri = new Uri("https://www.1a.ee/ru/otsing/?q=korpused");
             await Browser.OpenAsync(uri);
+        }
+        void tapImage_Tapped(object sender, EventArgs e)
+        {    
+            DisplayAlert("Корпус", "Корпус ПК является важным элементом, на котором крепятся все его устройства и защищающий их от воздействия окружающей среды.", "OK");
+        } 
+        void tapImage1_Tapped(object sender, EventArgs e)
+        {    
+            DisplayAlert("Материнская Плата", "Материнская плата — печатная плата, являющаяся основой построения модульного устройства, например — компьютера.", "OK");
+        }
+        void tapImage2_Tapped(object sender, EventArgs e)
+        {  
+             DisplayAlert("Видеокарта", "Видеокарта – устройство, преобразующее графический образ, хранящийся как содержимое памяти компьютера, в форму, пригодную для дальнейшего вывода на экран монитора.", "OK");
+        }
+        void tapImage3_Tapped(object sender, EventArgs e)
+        {  
+             DisplayAlert("Процессор", "Центральный процессор — электронный блок либо интегральная схема, исполняющая машинные инструкции, главная часть аппаратного обеспечения компьютера или программируемого логического контроллера.", "OK");
+        }
+        void tapImage4_Tapped(object sender, EventArgs e)
+        {  
+             DisplayAlert("ОЗУ", "Оперативная память или операти́вное запомина́ющее устро́йство — энергозависимая часть системы компьютерной памяти, в которой во время работы компьютера хранится выполняемый машинный код, а также входные, выходные и промежуточные данные, обрабатываемые процессором.", "OK");
+        }
+        void tapImage5_Tapped(object sender, EventArgs e)
+        {  
+             DisplayAlert("HDD", "Жёсткий диск — запоминающее устройство произвольного доступа, основанное на принципе магнитной записи. Является основным накопителем данных в большинстве компьютеров.", "OK");
         }
     }
 };
